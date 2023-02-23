f6a24f918f38ec00e4b8646091c70416385f9a57
SkyEngineerME <75652796+SkyEngineerME@users.noreply.github.com>
2023-02-23 18:08:57 +0700

    Done

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
+	
 };
 
-double overlap(){
-
-
+double overlap(Rect R1,Rect R2){
+    double R,L,UM,Um;
+	R = min((R1.x)+R1.w,(R2.x)+R2.w);
+	L = max(R1.x,R2.x);
+	Um = min(R1.y,R2.y);
+	UM = max((R1.y)-R1.h,(R2.y)-R2.h);
+	double A = (R-L)*(Um-UM);
+	if(A>0){
+		return A;
+	}else{
+		return 0;
+	}
 
 }
+
